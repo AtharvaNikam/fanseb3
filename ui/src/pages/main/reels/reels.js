4c0656d901d39648c3cb88dee6997aca640048b9
RakhKaran <132744751+RakhKaran@users.noreply.github.com>
2024-11-23 12:08:46 +0530

    "changes done"

--- ui/src/pages/main/reels/reels.js
+++ ui/src/pages/main/reels/reels.js
@@ -3,19 +3,19 @@ import { useRouter } from 'src/routes/hook';
 import { InfluencerView } from 'src/sections/main/influencer/view';
 import { useEffect } from 'react';
 import { useCheckout } from 'src/sections/hooks';
-// sections
-
-// ----------------------------------------------------------------------
 
 export default function ReelsPage() {
   const { checkout } = useCheckout();
   const { currentReelId } = checkout;
   const router = useRouter();
+
   useEffect(() => {
-    if (currentReelId) {
+    // Ensure `currentReelId` is valid before pushing to a new route
+    if (currentReelId && typeof currentReelId === 'string') {
       router.push(`/reels/${currentReelId}`);
     }
   }, [currentReelId, router]);
+
   return (
     <>
       <Helmet>
